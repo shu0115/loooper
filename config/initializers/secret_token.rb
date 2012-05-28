fb21df420c9edd4ce907fa0f08efc318b832f705
shu_0115 <raisondetre0115@gmail.com>
2012-05-28 12:27:25 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Loooper::Application.config.secret_token = '6b9755fefa7287fc58006cf1062cd3aca94ce0b3ecfb3fddd5c7f7abb2c44fda70161673b4fcec1f597bc984e1332db2beffd8f6cc93611755181c2cb2a58eb1'
