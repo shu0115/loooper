69e1194dbbd44944f6694aa2b631648a64baa40e
shu0115 <raisondetre0115@gmail.com>
2012-06-01 18:26:42 +0900

    アイテム名を表示するよう修正

--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -7,7 +7,7 @@ class HistoriesController < ApplicationController
   def index
     item_id = params[:item_id]
 
-    histories = History.where( user_id: session[:user_id] ).includes( :user ).order( "done_at DESC" ).limit(1000)
+    histories = History.where( user_id: session[:user_id] ).includes( :user, :item ).order( "done_at DESC" ).limit(1000)
 
     unless item_id.blank?
       histories = History.where( item_id: item_id )
