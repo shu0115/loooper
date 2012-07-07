c7f3164d84ea4718f19345ea6de4e8f0b8b53b14
teppei machida <machidanohimitus@gmail.com>
2012-07-07 17:39:52 +0900

    デザイン準備中

--- app/models/history.rb
+++ app/models/history.rb
@@ -1,21 +1,8 @@
 # coding: utf-8
-class HistoriesController < ApplicationController
+class History < ActiveRecord::Base
+  attr_accessible :done_at, :item_id, :user_id, :group_id
 
-  #-------#
-  # index #
-  #-------#
-  def index
-    item_id = params[:item_id]
-
-    histories = History.order( "done_at DESC" ).includes( :user, :item ).page( params[:page] ).per( 100 )
-
-    unless item_id.blank?
-      histories = histories.where( item_id: item_id )
-    else
-      histories = histories.where( user_id: session[:user_id] )
-    end
-
-    @histories = histories.all
-  end
+  belongs_to :user
+  belongs_to :item
 
 end
